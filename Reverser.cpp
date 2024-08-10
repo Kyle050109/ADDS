2eca481750239b1efea208e3af7a84cea1d82c34
Kyle050109 <161389102+Kyle050109@users.noreply.github.com>
2024-08-10 17:43:40 +0930

    Add files via upload

---
+++ Reverser.cpp
@@ -0,0 +1,26 @@
+#include "Reverser.h"
+
+int Reverser::reverseDigit(int value) {
+    if (value<0){
+        return -1;
+    }
+    
+    int reversed=0;
+
+    while (value>0){
+        reversed=reversed*10+(value%10);
+        value/=10;
+    }
+    return reversed;
+}
+
+
+std::string Reverser::reverseString(std::string characters){
+    if (characters.empty()){
+        return "ERROR";
+    }
+    if (characters.length()<=1){
+        return characters;
+    }
+    return reverseString(characters.substr(1))+csharacters[0];
+}
